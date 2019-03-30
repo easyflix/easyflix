60046cf168b8593b5eb8226a4b558d1357158d4d
Thomas Gambet <thomas.gambet@gmail.com>
2019-03-30 15:50:45 +0100

    top-nav component menu button

--- web/src/app/components/top-nav/top-nav.component.ts
+++ web/src/app/components/top-nav/top-nav.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {CoreService} from '@app/services/core.service';
 
 @Component({
   selector: 'app-top-nav',
@@ -8,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
 export class TopNavComponent implements OnInit {
 
   searchFocused = false;
+  showMenuButton$: Observable<boolean>;
 
-  constructor() { }
+  constructor(private core: CoreService) {
+    this.showMenuButton$ = core.getShowSidenav().pipe(map(b => !b));
+  }
 
   ngOnInit() {
+  }
+
+  openSidenav() {
+    this.core.openSidenav();
   }
 
 }
