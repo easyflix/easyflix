748015f7a1e705a9195cadcc5c7355e35b2934f9
Thomas Gambet <thomas.gambet@gmail.com>
2019-04-16 16:05:30 +0200

    format for LibraryFile

--- src/main/scala/net/creasource/webflix/LibraryFile.scala
+++ src/main/scala/net/creasource/webflix/LibraryFile.scala
@@ -2,4 +2,11 @@ package net.creasource.webflix
 
 import java.nio.file.Path
 
+import net.creasource.json.JsonSupport
+import spray.json._
+
 case class LibraryFile(name: String, path: Path, isDirectory: Boolean, size: Long, lastModified: Long)
+
+object LibraryFile extends JsonSupport {
+  implicit val format: RootJsonFormat[LibraryFile] = jsonFormat5(LibraryFile.apply)
+}
