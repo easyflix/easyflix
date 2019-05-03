2ecacec91486ab47c9c55bc4296c69faea846dac
Thomas Gambet <thomas.gambet@gmail.com>
2019-05-03 15:34:25 +0200

    deprecation: use scheduled instead of 'of'

--- web/src/app/services/service-helper.ts
+++ web/src/app/services/service-helper.ts
@@ -1,5 +1,5 @@
 import {Actions, ofType} from '@ngrx/effects';
-import {defer, Observable, of} from 'rxjs';
+import {asapScheduler, defer, Observable, scheduled} from 'rxjs';
 import {map, mergeMap, take} from 'rxjs/operators';
 import {Action, Store} from '@ngrx/store';
 import {State} from '@app/reducers';
@@ -9,7 +9,7 @@ export abstract class ServiceHelper {
   protected constructor(protected store: Store<State>, private actions$: Actions) {}
 
   protected dispatchActionObservable<T>(action: Action, success: string, error: string): Observable<T> {
-    return defer(() => of(this.store.dispatch(action))).pipe(
+    return defer(() => scheduled([this.store.dispatch(action)], asapScheduler)).pipe(
       mergeMap(() => this.actions$.pipe(
         ofType(success, error),
         take(1),
