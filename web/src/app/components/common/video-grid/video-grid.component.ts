26a0bc2d5712534ce93559972717969d223cf6ac
Thomas Gambet <thomas.gambet@gmail.com>
2019-05-08 21:47:40 +0200

    show poster on /movies

--- web/src/app/components/common/video-grid/video-grid.component.ts
+++ web/src/app/components/common/video-grid/video-grid.component.ts
@@ -1,4 +1,11 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {CoreService} from '@app/services/core.service';
+import {MoviesService} from '@app/services/movies.service';
+import {config, Observable} from 'rxjs';
+import {Movie} from '@app/models';
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
+import {filter, take} from 'rxjs/operators';
+import {ImagesConfig} from '@app/models/images-config';
 
 @Component({
   selector: 'app-video-grid',
@@ -8,9 +15,30 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class VideoGridComponent implements OnInit {
 
-  constructor() { }
+  movies$: Observable<Movie[]>;
+  config: ImagesConfig;
+
+  constructor(
+    private core: CoreService,
+    private movies: MoviesService,
+    private sanitizer: DomSanitizer,
+  ) {
+    this.movies$ = movies.getAll();
+    this.core.getConfig().pipe(filter(s => !!s), take(1)).subscribe(
+      conf => this.config = conf
+    );
+  }
 
   ngOnInit() {
+
+  }
+
+  getStyle(movie: Movie): SafeStyle {
+    if (config !== null) {
+      return this.sanitizer.bypassSecurityTrustStyle(
+        `background-image: url(${this.config.secure_base_url}w300${movie.poster})`
+      );
+    }
   }
 
 }
