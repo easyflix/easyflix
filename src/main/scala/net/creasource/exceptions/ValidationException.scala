0d38892d183dce7e663423d238078e29fc4f38c6
Thomas Gambet <thomas.gambet@gmail.com>
2019-05-11 22:05:57 +0200

    removed companion

--- src/main/scala/net/creasource/exceptions/ValidationException.scala
+++ src/main/scala/net/creasource/exceptions/ValidationException.scala
@@ -1,7 +1,3 @@
 package net.creasource.exceptions
 
-case class ValidationException(control: String, code: String, value: Option[String]) extends RuntimeException(s"Control $control is invalid: $code")
-
-object ValidationException {
-  def apply(control: String, code: String): ValidationException = apply(control, code, None)
-}
+case class ValidationException(control: String, code: String, value: Option[String] = None) extends RuntimeException(s"Control $control is invalid: $code")
