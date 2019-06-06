056ff63c175fb55048ccaa7bb971fadb91fd1b53
Thomas Gambet <thomas.gambet@gmail.com>
2019-06-06 19:43:20 +0200

    logger config

--- src/test/scala/net/creasource/MainTest.scala
+++ src/test/scala/net/creasource/MainTest.scala
@@ -1,15 +1,12 @@
 package net.creasource
 
 import net.creasource.util.SimpleTest
-//import net.creasource.web.LibraryActor.{GetLibraryFiles, ScanLibrary}
 
 class MainTest extends SimpleTest {
 
   "Webflix" should {
 
     "a" in {
-
-
 
     }
 
